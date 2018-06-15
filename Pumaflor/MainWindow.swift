eaed43462efe4513ba1908cf631977f94d2308e4
harrytwright <haroldtomwright@gmail.com>
2018-06-15 16:50:01 +0100

    Toolbar & Basic UI added

--- Pumaflor/MainWindow.swift
+++ Pumaflor/MainWindow.swift
@@ -8,12 +8,155 @@
 
 import Cocoa
 
-class MainWindow: NSWindow {
+extension NSToolbarItem.Identifier {
 
-    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
-        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+    static var mainSidebarController = NSToolbarItem.Identifier(rawValue: "MainWindowSideBarController")
+
+    static var websocketConnection = NSToolbarItem.Identifier(rawValue: "WebsocketConnection")
+
+    static var progressItem = NSToolbarItem.Identifier(rawValue: "ProgressItem")
+
+}
+
+extension NSToolbarItem {
+
+    static var progressIdicator: NSToolbarItem = {
+        let indicator = NSProgressIndicator(frame: NSRect(x: 0, y: 0, width: 20, height: 20))
+        indicator.style = .spinning
+        indicator.startAnimation(nil)
+
+        let item = NSToolbarItem(itemIdentifier: .progressItem)
+        item.label = "Connecting"
+        item.view = indicator
+
+        return item
+    }()
+
+}
+
+extension NSImage {
+
+    struct DefaultName: RawRepresentable {
+
+        typealias RawValue = String
+
+        var rawValue: String
+
+        init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        static var goLeftTemplate: DefaultName = DefaultName(rawValue: NSImage.goLeftTemplateName)
+
+        static var goRightTemplate: DefaultName = DefaultName(rawValue: NSImage.goRightTemplateName)
+
+        static var statusAvailable: DefaultName = DefaultName(rawValue: NSImage.statusAvailableName)
+
+        static var statusPartiallyAvailable: DefaultName = DefaultName(rawValue: NSImage.statusPartiallyAvailableName)
+
+        static var statusUnavailable: DefaultName = DefaultName(rawValue: NSImage.statusUnavailableName)
+
+        static var statusNone: DefaultName = DefaultName(rawValue: NSImage.statusNoneName)
+
+    }
+
+    convenience init(default name: DefaultName) {
+        self.init(named: name.rawValue)!
+    }
+
+}
+
+class MainWindow: NSWindow, NSToolbarDelegate {
+
+    lazy var toolbarIdentifier: [NSToolbarItem.Identifier] = {
+        return [.flexibleSpace, .space, .mainSidebarController, .websocketConnection, .progressItem]
+    }()
+
+    override init(
+        contentRect: NSRect,
+        styleMask style: StyleMask,
+        backing backingStoreType: BackingStoreType,
+        defer flag: Bool
+        )
+    {
+        super.init(
+            contentRect: contentRect,
+            styleMask: style,
+            backing: backingStoreType,
+            defer: flag
+        )
 
         self.titleVisibility = .hidden
+        commonInit()
+    }
+
+    func commonInit() {
+        let toolbar = NSToolbar(identifier: "MainWindowToolBar")
+        toolbar.allowsUserCustomization.toggle()
+        toolbar.delegate = self
+
+        self.toolbar = toolbar
+    }
+
+    func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        let toolbarItem: NSToolbarItem
+
+        if itemIdentifier == .mainSidebarController {
+            let group = NSToolbarItemGroup(itemIdentifier: itemIdentifier)
+
+            let itemA = NSToolbarItem(itemIdentifier: NSToolbarItem.Identifier(rawValue: "LeftSideItem"))
+            let itemB = NSToolbarItem(itemIdentifier: NSToolbarItem.Identifier(rawValue: "RightSideItem"))
+
+            let segmented = NSSegmentedControl(frame: NSRect(x: 0, y: 0, width: 85, height: 40))
+            segmented.segmentStyle = .texturedRounded
+            segmented.trackingMode = .selectAny
+            segmented.segmentCount = 2
+
+            // Don't set a label: these would appear inside the button
+            segmented.setImage(NSImage(default: .goLeftTemplate), forSegment: 0)
+            segmented.setWidth(40, forSegment: 0)
+            segmented.setImage(NSImage(default: .goRightTemplate), forSegment: 1)
+            segmented.setWidth(40, forSegment: 1)
+
+            // `group.label` would overwrite segment labels
+            group.paletteLabel = "Navigation"
+            group.subitems = [itemA, itemB]
+            group.view = segmented
+
+            toolbarItem = group
+        } else if itemIdentifier == .websocketConnection {
+            toolbarItem = NSToolbarItem(itemIdentifier: itemIdentifier)
+            toolbarItem.label = "Connection"
+            toolbarItem.toolTip = "Connection status"
+
+            let iconImage = NSImage(default: .statusNone)
+
+            let button = NSButton(frame: NSRect(x: 0, y: 0, width: 40, height: 40))
+            button.title = ""
+            button.image = iconImage
+            button.bezelStyle = .texturedRounded
+            button.setButtonType(.momentaryLight)
+            
+            toolbarItem.view = button
+        } else if itemIdentifier == .progressItem {
+            toolbarItem = .progressIdicator
+        } else {
+            toolbarItem = NSToolbarItem(itemIdentifier: itemIdentifier)
+        }
+
+        return toolbarItem
+    }
+
+    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return toolbarAllowedItemIdentifiers(toolbar)
+    }
+
+    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return self.toolbarIdentifier
+    }
+
+    func toolbarSelectableItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return self.toolbarDefaultItemIdentifiers(toolbar)
     }
 
 }
--- Pumaflor/ViewController.swift
+++ Pumaflor/ViewController.swift
@@ -8,12 +8,69 @@
 
 import Cocoa
 
+struct Customer {
+    var id: UUID
+    var name: String
+}
+
+struct Sale {
+    var id: UUID
+    var customer: Customer
+    var reference: String
+}
+
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var tableView: NSTableView!
+
+    var sales: [Sale] = [] {
+        didSet {
+            DispatchQueue.main.sync {
+                tableView.reloadData()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+
+    @discardableResult
+    override func presentError(_ error: Error) -> Bool {
+        return DispatchQueue.main.sync {
+            return super.presentError(error)
+        }
+    }
+
+    override func viewDidAppear() {
+
+        let request = URLRequest(url: URL(string: "http://www.apple.com")!)
+
+        let queue = DispatchQueue.main
+        let task = URLSession.shared.dataTask(with: request) { (_, _, error) in
+            defer { queue.sync { self.view.window?.toolbar?.removeItem(at: 4) } }
+
+            let _items = queue.sync { self.view.window?.toolbar?.items }
+            guard error == nil, let items = _items else { self.presentError(error!); return }
+
+            for item in items where item.itemIdentifier == .websocketConnection {
+                queue.sync {
+                    item.image = NSImage(default: .statusAvailable)
+                }
+            }
+
+            let customer = Customer(id: UUID(), name: "Aurélien Géron")
+            let newSale = Sale(id: UUID(), customer: customer, reference: "066681")
+            self.sales.append(newSale)
+        }
+
+        task.resume()
     }
 
     override var representedObject: Any? {
@@ -22,6 +79,48 @@ class ViewController: NSViewController {
         }
     }
 
+}
 
+extension ViewController: NSTableViewDataSource {
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return sales.count
+    }
+
+
+}
+
+extension ViewController: NSTableViewDelegate {
+
+    fileprivate enum CellIdentifiers {
+        static let kIDCell = "IDCellIdentifier"
+        static let kCustomerCell = "CustomerCellIdentifier"
+        static let kReferenceCell = "ReferenceCellIdentifier"
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let item = sales[row]
+
+        var value: String
+        var cellIdentifier: String
+
+        if tableColumn == tableView.tableColumns[0] {
+            value = item.id.uuidString
+            cellIdentifier = CellIdentifiers.kIDCell
+        } else if tableColumn == tableView.tableColumns[1] {
+            value = item.customer.name
+            cellIdentifier = CellIdentifiers.kCustomerCell
+        } else {
+            value = item.reference
+            cellIdentifier = CellIdentifiers.kReferenceCell
+        }
+
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = value
+            return cell
+        }
+
+        return nil
+    }
 }
 
